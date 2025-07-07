582652f98a47ecef4e95026ac0402b4745ee8d1d
Samir Chaudhary <datascience325325@gmail.com>
2025-07-07 17:22:23 +0545

    add name

--- BasicJava.java
+++ BasicJava.java
@@ -2,6 +2,6 @@ public class BasicJava{
     public static void main(String[] args) {
         
         System.out.println("First program in java");
-        System.out.println("Hello World");
+        System.out.println("Hello World i'm samir");
     }
 }
